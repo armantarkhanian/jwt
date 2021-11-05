43be58461d8112d76708ff8f27f4d2ff04355913
Arman Tarkhanian <armantarkhanian@gmail.com>
2021-11-05 22:22:36 +0300

    auto commit

--- jwt.go
+++ jwt.go
@@ -7,35 +7,28 @@ import (
 	jwtv3 "github.com/gbrlsnchs/jwt/v3"
 )
 
-type EncodeDecoder interface {
-	Encode(interface{}) (string, error)
-	Decode(string, interface{}) error
-}
-
-type encodeDecoder struct {
+type EncodeDecoder struct {
 	alg *jwtv3.HMACSHA
 }
 
-var _ EncodeDecoder = &encodeDecoder{}
-
 var ErrInvalidKey = errors.New("jwt: invalid HMAC secret key")
 
-func NewEncodeDecoder(secretKey string) (EncodeDecoder, error) {
+func New(secretKey string) (*EncodeDecoder, error) {
 	if utf8.RuneCountInString(secretKey) < 32 {
 		return nil, ErrInvalidKey
 	}
 
-	return &encodeDecoder{
+	return &EncodeDecoder{
 		alg: jwtv3.NewHS256([]byte(secretKey)),
 	}, nil
 }
 
-func (ed encodeDecoder) Encode(payload interface{}) (string, error) {
+func (ed EncodeDecoder) Encode(payload interface{}) (string, error) {
 	token, err := jwtv3.Sign(payload, ed.alg)
 	return string(token), err
 }
 
-func (ed encodeDecoder) Decode(token string, payload interface{}) error {
+func (ed EncodeDecoder) Decode(token string, payload interface{}) error {
 	_, err := jwtv3.Verify([]byte(token), ed.alg, payload)
 	return err
 }
